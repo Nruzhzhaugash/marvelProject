6655c452164d1a30349be91d5084b4b75225b235
Nruzhzhaugash <clonn262@gmail.com>
2023-11-28 03:17:23 +0600

    add spinner and error message

--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -12,6 +12,8 @@ class MarvelService {
       return await res.json()
    }
 
+   
+
    getAllCharacters = async () => {
       const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
       return res.data.results.map(this._transformCharacter)
@@ -25,12 +27,13 @@ class MarvelService {
    _transformCharacter = (char) => {
       return {
          name: char.name,
-         description: char.description,
+         description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
          thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
          homepage: char.urls[0].url,
          wiki: char.urls[1].url
       }
    }
+   
 }
 
 export default MarvelService
