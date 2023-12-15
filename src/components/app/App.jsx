c805fa0649b46cb120aa86313355ebaf781be2a4
Nruzhzhaugash <clonn262@gmail.com>
2023-12-15 16:54:03 +0600

    lazy router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,13 @@
+import {lazy, Suspense} from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { MainPage, ComicsPage, NoMatch, SingleComicPage } from '../pages';
 import AppHeader from "../appHeader/AppHeader";
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+
+const NoMatch = lazy(() => import('../pages/NoMatch'))
+const MainPage = lazy(() => import('../pages/MainPage'))
+const ComicsPage = lazy(() => import('../pages/ComicsPage'))
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'))
 
 const App = () => {
 
@@ -10,12 +16,14 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route path='/' element={<MainPage />} />
-                        <Route path='/comics' element={<ComicsPage />} />
-                        <Route path='/comics/:comicId' element={<SingleComicPage />} />
-                        <Route path='*' element={<NoMatch />} />
-                    </Routes>
+                    <Suspense fallback={<ErrorBoundary />}>
+                        <Routes>
+                            <Route path='/' element={<MainPage />} />
+                            <Route path='/comics' element={<ComicsPage />} />
+                            <Route path='/comics/:comicId' element={<SingleComicPage />} />
+                            <Route path='*' element={<NoMatch />} />
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
